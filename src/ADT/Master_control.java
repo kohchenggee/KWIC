cbf59db471e0671d6c0c1b6353202ddf2d991f98
kohchenggee <kohchenggee@gmail.com>
2016-08-26 19:21:54 +0800

    Merging 2 architecture Design in Main

--- src/ADT/Master_control.java
+++ src/ADT/Master_control.java
@@ -1,10 +1,11 @@
+//implement by Phoon Qianonn
 package ADT;
 
 public class Master_control {
 	
 	public static String[] ignoreWords = {"is", "the", "of", "and", "as", "a", "after"};
 	
-	public static void main(String[] args) {
+	public static void run() {
 		// TODO Auto-generated method stub
 		Input i = new Input(); 
 		Circular_shift c = new Circular_shift();
--- src/Main.java
+++ src/Main.java
@@ -1,74 +1,29 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
+
+import ADT.Input;
+import ADT.Master_control;
+import SDS.SharedDataSolution;
 public class Main {
 
 	public static String[] ignoreWords = {"is", "the", "of", "and", "as", "a", "after"};
 	public static void main(String[] args) {
-		ArrayList<String> msgArray = new ArrayList<String>();
+		//ArrayList<String> msgArray = new ArrayList<String>();
 		Scanner sc = new Scanner(System.in);
-		int input = numOfInputs(sc);// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
 		
-		msgArray=filterIgnoreWords( ignoreWords, sc, input);
-		ArrayList<String> msgArray2=msgArray;
-		Collections.sort(msgArray2);
-		printMsg(msgArray2);
-	}
-
-	private static void printMsg(ArrayList<String> msgArray) {
-		for(int i=0;i<msgArray.size();i++){
-			System.out.println(msgArray.get(i));
-		}
-	}
-
-	private static int numOfInputs(Scanner sc) {
-		System.out.println("Number of Input");
+		System.out.println("Press 1 to run with Shared Data Solution/nPress 2 to run with ADT");
 		String num=sc.nextLine();
 		int input = Integer.parseInt(num);
-		return input;
-	}
-
-	private static ArrayList<String> filterIgnoreWords( String[] ignoreWords, Scanner sc, int input) {
-		ArrayList<String> msgArray = new ArrayList<String>();
-		String input2;
-		for(int i=0;i<input;i++){
-
-			input2=sc.nextLine();
-			String[] split = input2.split(" ");
-			for(int j=0;j<split.length;j++){
-				String msg = "";
-				boolean flag=false;
-				flag = checkIgnoreWords(ignoreWords, split, j, flag);
-				if(flag==false){
-					addStringtoArrayList(msgArray, split, j, msg);
-				}
-			}
+		if(input==1){
+		SharedDataSolution.runSSD();
+		}else if(input==2){
+		Master_control.run();
 		}
-		return msgArray;
 	}
 
-	private static boolean checkIgnoreWords(String[] ignoreWords, String[] split, int j, boolean flag) {
-		for(int k=0;k<ignoreWords.length;k++){
-			if(split[j].equalsIgnoreCase(ignoreWords[k])){
-				flag=true;
-				break;
-			}
-		}
-		return flag;
-	}
-
-	private static void addStringtoArrayList(ArrayList<String> msgArray, String[] split, int j, String msg) {
-		msg+=split[j];
-		for(int l=1;l<split.length;l++){
-			int add=j+l;
-			if(add>=split.length){
-				add=add-split.length;
-			}
-			msg+=" "+split[add];
-			
-		}
-		msgArray.add(msg);
-	}
+	
 
 }
 
