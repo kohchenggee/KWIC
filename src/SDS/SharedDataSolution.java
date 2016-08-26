6703cbb9c56c089c3801c82ad157ddc7768e95c8
kohchenggee <kohchenggee@gmail.com>
2016-08-26 20:11:08 +0800

    Remove number of line

--- src/SDS/SharedDataSolution.java
+++ src/SDS/SharedDataSolution.java
@@ -11,12 +11,9 @@ public class SharedDataSolution {
 	public static void runSSD(){
 	ArrayList<String> msgArray = new ArrayList<String>();
 	Scanner sc = new Scanner(System.in);
-	int input = numOfInputs(sc);
-	if(input>0){
-	msgArray=filterIgnoreWords(ignoreWords,sc,input);
-	}else{
-		msgArray=filterIgnoreWords2(ignoreWords,sc);
-	}
+
+		msgArray=filterIgnoreWords(ignoreWords,sc);
+	
 	Collections.sort(msgArray);
 	printMsg(msgArray);
 	}
@@ -26,35 +23,13 @@ public class SharedDataSolution {
 		}
 	}
 
-	private static int numOfInputs(Scanner sc) {
-		System.out.println("Number of Inputs. Please insert 0 if not sure of the number of lines.");
-		String num=sc.nextLine();
-		int input = Integer.parseInt(num);
-		return input;
-	}
 
-	private static ArrayList<String> filterIgnoreWords( String[] ignoreWords, Scanner sc, int input) {
+
+
+	private static ArrayList<String> filterIgnoreWords( String[] ignoreWords, Scanner sc) {
 		ArrayList<String> msgArray = new ArrayList<String>();
 		String input2;
-		for(int i=0;i<input;i++){
-
-			input2=sc.nextLine();
-			String[] split = input2.split(" ");
-			for(int j=0;j<split.length;j++){
-				String msg = "";
-				boolean flag=false;
-				flag = checkIgnoreWords(ignoreWords, split, j, flag);
-				if(flag==false){
-					addStringtoArrayList(msgArray, split, j, msg);
-				}
-			}
-		}
-		return msgArray;
-	}
-	private static ArrayList<String> filterIgnoreWords2( String[] ignoreWords, Scanner sc) {
-		ArrayList<String> msgArray = new ArrayList<String>();
-		String input2;
-		System.out.println("System will display the list when enter Empty String.");
+		System.out.println("System will display the list when enter Empty Line.");
 		input2=sc.nextLine();
 		while(!input2.equals("")){
 			String[] split = input2.split(" ");
